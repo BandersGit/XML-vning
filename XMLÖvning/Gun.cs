9c79e3f7e4d73d2dcb0c890b6c39ea959c0a3325
BandersGit <andy.i.flaskan@gmail.com>
2020-09-23 10:44:33 +0200

    User can write own answers and they are saved

---
+++ XMLÖvning/Gun.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace XMLÖvning
+{
+    public class Gun
+    {
+        public string name = "";
+
+        public string dmg = "";
+
+        public string reloadSpeed = "";
+    }
+}
--- XMLÖvning/Program.cs
+++ XMLÖvning/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace XMLÖvning
 {
@@ -6,7 +8,27 @@ namespace XMLÖvning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlSerializer gunSerializer = new XmlSerializer (typeof(Gun));
+
+
+            Gun smg = new Gun();
+
+            System.Console.WriteLine("Name your gun!");
+            smg.name = Console.ReadLine();
+
+            System.Console.WriteLine("Now set it's damage");
+            smg.dmg = Console.ReadLine();
+
+            System.Console.WriteLine("Finally, you need to set it's reload speed");
+            smg.reloadSpeed = Console.ReadLine();
+
+
+            FileStream file = File.Open(@"gun.xml", FileMode.OpenOrCreate);
+
+            gunSerializer.Serialize(file, smg);
+
+            file.Close();
+
         }
     }
 }
